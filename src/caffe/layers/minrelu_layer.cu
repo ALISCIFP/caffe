77f0eae80287b4e4e0a80daf74a549c9582e44cf
Zengming(Zack) Shen <szm0219@gmail.com>
2016-10-14 18:04:27 -0500

    fix bug for minrelu cuda implementation

--- src/caffe/layers/minrelu_layer.cu
+++ src/caffe/layers/minrelu_layer.cu
@@ -6,7 +6,7 @@
 namespace caffe {
 
 template <typename Dtype>
-__global__ void ReLUForward(const int n, const Dtype* in, Dtype* out,
+__global__ void MinReLUForward(const int n, const Dtype* in, Dtype* out,
     Dtype positive_slope) {
   CUDA_KERNEL_LOOP(index, n) {
     out[index] = in[index] < 0 ? in[index] : in[index] * positive_slope;
@@ -19,9 +19,9 @@ void MinReLULayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
   const int count = bottom[0]->count();
-  Dtype positive_slope = this->layer_param_.relu_param().positive_slope();
+  Dtype positive_slope = this->layer_param_.minrelu_param().positive_slope();
   // NOLINT_NEXT_LINE(whitespace/operators)
-  ReLUForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+  MinReLUForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
       count, bottom_data, top_data, positive_slope);
   CUDA_POST_KERNEL_CHECK;
   // << " count: " << count << " bottom_data: "
@@ -32,7 +32,7 @@ void MinReLULayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
 }
 
 template <typename Dtype>
-__global__ void ReLUBackward(const int n, const Dtype* in_diff,
+__global__ void MinReLUBackward(const int n, const Dtype* in_diff,
     const Dtype* in_data, Dtype* out_diff, Dtype positive_slope) {
   CUDA_KERNEL_LOOP(index, n) {
     out_diff[index] = in_diff[index] * ((in_data[index] < 0)
@@ -49,9 +49,9 @@ void MinReLULayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     const Dtype* top_diff = top[0]->gpu_diff();
     Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
     const int count = bottom[0]->count();
-    Dtype positive_slope = this->layer_param_.relu_param().positive_slope();
+    Dtype positive_slope = this->layer_param_.minrelu_param().positive_slope();
     // NOLINT_NEXT_LINE(whitespace/operators)
-    ReLUBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+    MinReLUBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
         count, top_diff, bottom_data, bottom_diff, positive_slope);
     CUDA_POST_KERNEL_CHECK;
   }
